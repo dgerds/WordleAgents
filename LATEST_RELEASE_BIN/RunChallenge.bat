5fb062039fe06c06e0257cd5d343c9e5b9e75f82
dgerds <damiengerhardy@gmail.com>
2022-04-04 15:30:36 +0930

    Updated RunChallenge script with last line summary results

--- LATEST_RELEASE_BIN/RunChallenge.bat
+++ LATEST_RELEASE_BIN/RunChallenge.bat
@@ -4,10 +4,12 @@ echo Picking words...
 PickWords.exe 5 > temp-file.tmp
 set /p WORD_LIST= < temp-file.tmp
 echo Word list: %WORD_LIST%
+del temp-file.tmp
 
 for %%f in (*.dll) do (
     echo.
     echo Starting: %%~nf
     PlayWordle.exe %%~nf.dll 5 %WORD_LIST% > %%~nf.out
     echo Complete: %%~nf.out
+    LastLine.exe %%~nf.out
 )
